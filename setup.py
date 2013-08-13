60089af5e04b56963de2c774362a5d82c6767527
Arthur Rio <arthur@punchtab.com>
2013-08-13 16:09:29 -0700

    add the readme and the setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='geoip-dat',
+    version='0.0.1',
+    author=u'Arthur Rio',
+    author_email='arthur@punchtab.com',
+    packages=find_packages(),
+    url='https://github.com/PunchTab/geo-ip',
+    license='BSD licence, see LICENCE',
+    description='.dat file for pygeoip',
+    long_description=open('README.md').read(),
+    zip_safe=False,
+)
+
